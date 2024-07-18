e3ff4a01ae3dba2771715d913fcac0ed2427f2ce
kpc-simone <kpc.simone@gmail.com>
2024-07-17 20:30:46 -0600

    metaplasticity with variance-dependent learning rate

---
+++ metaplasticity/metaplasticity_windowed_variance_current_injection.py
@@ -0,0 +1,93 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import nengo
+
+class Learner(nengo.processes.Process):
+    def __init__(self, size_in, size_out, tau_learn):
+        self.size_in = size_in
+        self.size_out = size_out
+        self.tau_learn = tau_learn   # convergence time of the learning process
+        super().__init__(default_size_in=size_in*2+size_out,
+                         default_size_out=size_out)
+        
+    def make_step(self, shape_in, shape_out, dt, rng, state=None):
+        w = np.zeros((self.size_out, self.size_in))
+        learn_scale = 1-np.exp(-dt/self.tau_learn)
+        
+        def step_learn(t, x, w=w, learn_scale=learn_scale):
+            pre, meta, error = x[:self.size_in], x[self.size_in:self.size_in*2],x[self.size_in*2:]
+            
+            # compute the learning rate that will give the desired convergence time
+            lr = np.sum(pre*pre*meta)
+            if lr != 0:
+                lr = 1.0 / lr
+            lr = learn_scale * lr
+            
+            w -= lr * np.outer(error, pre*meta)            
+            
+            return w @ pre
+        return step_learn
+        
+class WindowedFunc(object):
+    def __init__(self,size_in,size_out,function='variance',window_size=10):
+        self.size_in = size_in
+        self.size_out = size_out
+        self.function = function
+        self.history = np.zeros((window_size,self.size_out))
+    
+    def __call__(self,t,x):
+        self.history = np.roll(self.history, shift=-1, axis=0)
+        self.history[-1,:] = x
+        
+        return np.var(self.history,axis=0)
+        
+
+n_neurons = 200
+
+model = nengo.Network()
+with model:
+    model.config[nengo.Connection].synapse=None
+    
+    stim = nengo.Node( nengo.processes.WhiteSignal(1.0, high=5, seed=0) )
+    
+    # change to present input
+    inj = nengo.Node( nengo.processes.PresentInput( inputs = [-1., 1.], presentation_time = 1.) )
+    
+    output = nengo.Node(None, size_in=1)
+    ens = nengo.Ensemble(n_neurons = n_neurons, 
+                        dimensions = 1,
+                        neuron_type = nengo.Tanh(),
+                        )
+    nengo.Connection(stim, ens)
+    nengo.Connection(inj,ens) 
+    
+    learn = nengo.Node( Learner( 
+                                    size_in = ens.n_neurons, 
+                                    size_out = stim.size_out,
+                                    tau_learn = 0.02)
+                                    )
+    error = nengo.Node(None, size_in=1)
+    nengo.Connection(ens.neurons, learn[:ens.n_neurons])
+    
+    meta = nengo.Node( WindowedFunc(size_in=n_neurons,size_out=n_neurons),size_in = n_neurons )
+    nengo.Connection(ens.neurons, meta, synapse=None)
+    nengo.Connection(meta, learn[ens.n_neurons:ens.n_neurons*2])
+    
+    nengo.Connection(learn, output)
+    
+    nengo.Connection(output, error)
+    nengo.Connection(stim, error, transform=-1)
+    nengo.Connection(error, learn[-stim.size_out:], synapse=0)
+    
+    p_error = nengo.Probe(error)
+
+if __name__ == "__main__":
+
+    sim = nengo.Simulator(model)
+    with sim:
+        sim.run(30)
+        
+    plt.figure(figsize=(14,3))
+    plt.plot(sim.trange(), np.linalg.norm(sim.data[p_error], axis=1), lw=1, label='error')
+    plt.legend()
+    plt.show()
